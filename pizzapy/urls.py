88442f2904231b010736d20c1eca1bc6d61b1c5d
jack-debug <55571956+jack-debug@users.noreply.github.com>
2019-10-14 13:43:38 +1100

    Update urls.py

--- pizzapy/urls.py
+++ pizzapy/urls.py
@@ -14,15 +14,15 @@ class Urls(object):
         self.country = country
         self.urls = {
             COUNTRY_USA: {
-                'find_url' : 'https://order.dominos.com/power/store-locator?s={line1}&c={line2}&type={type}',
-                'info_url' : 'https://order.dominos.com/power/store/{store_id}/profile',
-                'menu_url' : 'https://order.dominos.com/power/store/{store_id}/menu?lang={lang}&structured=true',
-                'place_url' : 'https://order.dominos.com/power/place-order',
-                'price_url' : 'https://order.dominos.com/power/price-order',
-                'track_by_order' : 'https://trkweb.dominos.com/orderstorage/GetTrackerData?StoreID={store_id}&OrderKey={order_key}',
-                'track_by_phone' : 'https://trkweb.dominos.com/orderstorage/GetTrackerData?Phone={phone}',
-                'validate_url' : 'https://order.dominos.com/power/validate-order',
-                'coupon_url' : 'https://order.dominos.com/power/store/{store_id}/coupon/{couponid}?lang={lang}',
+                'find_url' : 'https://order.dominos.com.au/power/store-locator?s={line1}&c={line2}&type={type}',
+                'info_url' : 'https://order.dominos.com.au/power/store/{store_id}/profile',
+                'menu_url' : 'https://order.dominos.com.au/power/store/{store_id}/menu?lang={lang}&structured=true',
+                'place_url' : 'https://order.dominos.com.au/power/place-order',
+                'price_url' : 'https://order.dominos.com.au/power/price-order',
+                'track_by_order' : 'https://trkweb.dominos.com.au/orderstorage/GetTrackerData?StoreID={store_id}&OrderKey={order_key}',
+                'track_by_phone' : 'https://trkweb.dominos.com.au/orderstorage/GetTrackerData?Phone={phone}',
+                'validate_url' : 'https://order.dominos.com.au/power/validate-order',
+                'coupon_url' : 'https://order.dominos.com.au/power/store/{store_id}/coupon/{couponid}?lang={lang}',
             },
             COUNTRY_CANADA: {
                 'find_url' : 'https://order.dominos.ca/power/store-locator?s={line1}&c={line2}&type={type}',
